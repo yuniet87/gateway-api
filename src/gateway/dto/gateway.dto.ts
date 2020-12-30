6d80c5e5fb96d1e2bc5e40299f48b22fba7e5a59
yuniet87 <yuniet87@gmail.com>
2020-12-30 12:06:54 -0500

    Creado el DTO

---
+++ src/gateway/dto/gateway.dto.ts
@@ -0,0 +1,6 @@
+export class CreateGatewayDTO {
+  readonly serialNumber: string;
+  readonly hrName: string;
+  readonly ip4Address: string;
+  readonly devices: any;
+}
--- src/gateway/gateway.controller.ts
+++ src/gateway/gateway.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { CreateGatewayDTO } from './dto/gateway.dto';
 
 @Controller('gateway')
 export class GatewayController {
   @Post('/create')
-  createPost(@Res() res) {
+  createPost(@Res() res, @Body() createGatewayDTO: CreateGatewayDTO) {
+    // console.log(createGatewayDTO);
     return res.status(HttpStatus.OK).json({
       message: 'received',
     });
